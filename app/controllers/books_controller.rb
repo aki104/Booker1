231cb5fc74769b232a9b55f6e3cd5555a6385159
akitoshi yamashita <aki1041990@gmail.com git config --global user.name aki104>
2018-08-17 14:04:08 +0900

    Finish 4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  	@book = Book.new
+  	# order(created_at: :desc) 降順にする
+    @books = Book.all.order(created_at: :desc)
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  	flash[:success] = 'Book was successfully created..'
+  	redirect_to book_path(@book.id)
+  	 else
+    	@books = Book.all.order(created_at: :desc)
+     	render :new
+     end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book.id)
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+    book.destroy
+    redirect_to new_book_path
+  end
+
+  private
+
+
+  def book_params
+	params.require(:book).permit(:title, :body)
+  end
+end
